156fd283473d2634bad01dbd5e3568061109dc32
BlueKossa <dexter@stormdal.se>
2025-11-23 03:30:28 +0100

    Added an api option

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,11 @@ use axum::{
     extract::State,
     routing::{get, post},
 };
+use clap::{Parser};
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use std::{
-    collections::HashMap,
-    fs,
-    net::IpAddr,
-    sync::Arc,
-    time::{SystemTime, UNIX_EPOCH},
+    collections::HashMap, fs, net::IpAddr, path::PathBuf, sync::Arc, time::{SystemTime, UNIX_EPOCH}
 };
 use tokio::sync::Mutex;
 use trust_dns_resolver::{TokioAsyncResolver, config::*};
@@ -31,15 +28,78 @@ struct HasJoinedQuery {
     serverId: String,
 }
 
-type SessionMap = Arc<Mutex<HashMap<String, HashMap<String, u64>>>>;
-type AccountMap = Arc<HashMap<String, String>>;
+#[derive(Debug, Deserialize, Serialize)]
+struct Session {
+    uuid: String,
+    servers: HashMap<String, u64>
+}
+
+type SessionMap = Arc<Mutex<HashMap<String, Session>>>;
+type AccountMap = Arc<AccountServerKind>;
+
+enum AccountServerKind {
+    API {
+        secret: Option<String>,
+        endpoint: String
+    },
+    File(HashMap<String, String>)
+}
+
+#[derive(Deserialize)]
+struct Res {
+    username: String
+}
+
+impl AccountServerKind {
+    async fn get(&self, k: &str) -> Option<String> {
+        match self {
+            AccountServerKind::API { secret, endpoint } => {
+                let client = Client::new();
+                let mut req = client
+                    .get(format!("{endpoint}?token={k}"));
+                if let Some(s) = secret {
+                    req = req.bearer_auth(s);
+                }
+                let resp = req.send().await;
+                let Ok(resp) = resp else {
+                    return None;
+                };
+                resp.json::<Res>().await.map(|r| r.username).ok()
+            }
+            AccountServerKind::File(hash_map) => {
+                hash_map.get(k).cloned()
+            }
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    accounts: Option<PathBuf>,
+
+    #[arg(short, long)]
+    sessions: Option<PathBuf>,
+
+    #[arg(short, long)]
+    endpoint: Option<String>,
+
+    #[arg(long)]
+    api: bool,
+}
 
 #[tokio::main]
 async fn main() {
-    let account_path = std::env::var("ACCOUNT_JSON").unwrap_or(String::from("accounts.json"));
-    let accounts: AccountMap =
-        Arc::new(serde_json::from_str(&fs::read_to_string(account_path).unwrap()).unwrap());
-    let sessions_path = std::env::var("SESSIONS_JSON").unwrap_or(String::from("sessions.json"));
+    let args = Args::parse();
+    let accounts = if args.api {
+        AccountServerKind::API { secret: None, endpoint: args.endpoint.unwrap() }
+    } else {
+        AccountServerKind::File(serde_json::from_str(&fs::read_to_string(args.accounts.unwrap()).unwrap()).unwrap())
+    };
+    let accounts = Arc::new(accounts);
+    let sessions_path = args.sessions.unwrap_or(PathBuf::from("sessions.json"));
     let sessions: SessionMap = Arc::new(Mutex::new(
         serde_json::from_str(&fs::read_to_string(sessions_path).unwrap()).unwrap_or_default(),
     ));
@@ -72,6 +132,7 @@ async fn join_handler(
     State((accounts, sessions)): State<(AccountMap, SessionMap)>,
     Json(payload): Json<JoinRequest>,
 ) -> axum::http::StatusCode {
+    println!("{} ({:?}) joining {}", &payload.selectedProfile, &payload.authString, &payload.serverId);
     let ip = resolve_mojang_ip().await;
     let url = format!("https://{}/session/minecraft/", ip);
     let client = Client::builder()
@@ -84,8 +145,8 @@ async fn join_handler(
         .as_secs();
 
     if let Some(auth) = payload.authString {
-        if let Some(stored) = accounts.get(&payload.selectedProfile) {
-            if stored == &auth {
+        if let Some(stored_profile) = accounts.get(&auth).await {
+            if stored_profile == payload.selectedProfile {
                 let profile: serde_json::Value = client
                     .get(format!("{}profile/{}", url, payload.selectedProfile))
                     .header("Host", "sessionserver.mojang.com")
@@ -100,18 +161,20 @@ async fn join_handler(
                     let username = name.to_lowercase();
                     let mut sessions = sessions.lock().await;
 
-                    sessions
-                        .entry(username.clone())
-                        .or_default()
-                        .retain(|_, &mut t| t >= now - 60);
+                    for session in sessions.values_mut() {
+                        session.servers.retain(|_, &mut t| now.saturating_sub(t) <= 60);
+                    }
 
                     sessions
                         .entry(username.clone())
-                        .or_default()
-                        .insert("uuid".to_string(), payload.selectedProfile.parse().unwrap());
+                        .or_insert(
+                            Session { uuid: payload.selectedProfile, servers: HashMap::new() }
+                        );
+
                     sessions
                         .get_mut(&username)
                         .unwrap()
+                        .servers
                         .insert(payload.serverId, now);
 
                     fs::write("sessions.json", serde_json::to_string(&*sessions).unwrap()).unwrap();
@@ -155,9 +218,9 @@ async fn has_joined_handler(
 
     let sessions = sessions.lock().await;
     if let Some(user_sessions) = sessions.get(&username) {
-        if let Some(&timestamp) = user_sessions.get(&query.serverId) {
+        if let Some(&timestamp) = user_sessions.servers.get(&query.serverId) {
             if timestamp >= now - 60 {
-                let uuid = &user_sessions["uuid"];
+                let uuid = &user_sessions.uuid;
                 let resp = client
                     .get(format!("{}profile/{}?unsigned=false", url, uuid))
                     .header("Host", "sessionserver.mojang.com")
